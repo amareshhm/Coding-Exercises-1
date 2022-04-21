183ef11ec7b9f83363fa78ace9a04e6b11a5ce81
amareshhm <amareshhm92@gmail.com>
2022-04-21 07:39:12 +0530

    Update SumOfNoInString.java

--- SumOfNoInString.java
+++ SumOfNoInString.java
@@ -1,26 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class SumOfNoInString {
+    static int findSum(String str) {
+        // A temporary string
+        StringBuilder temp = new StringBuilder("0");
 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str;
-		int cases = Integer.parseInt(br.readLine());
-		while(cases-- > 0)
-		{
-			str = br.readLine();
-			String[] s = str.split("\\D");
-			int sum = 0;
-			for(int i=0;i<s.length;i++)
-			{
-				if(!s[i].isEmpty())
-				sum += Integer.parseInt(s[i]);
-			}
-			System.out.println(sum);
-		}
-	}
+        // holds sum of all numbers present in the string
+        int sum = 0;
 
+        // read each character in input string
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            // if current character is a digit
+            if (Character.isDigit(ch)) {
+                temp.append(ch);
+            }
+            // if current character is an alphabet
+            else {
+                // increment sum by number found earlier
+                // (if any)
+                sum += Integer.parseInt(temp.toString());
+                System.out.println(sum);
+
+                // reset temporary string to empty
+                temp = new StringBuilder("0");
+            }
+        }
+
+        // takes care of trailing numbers - ex: 68
+        return sum + Integer.parseInt(temp.toString());
+    }
+
+    // Driver code
+    public static void main(String[] args) {
+
+        // input alphanumeric string
+        String str = "12abc20yz68";
+
+        // Function call
+        System.out.println(findSum(str));
+    }
 }
