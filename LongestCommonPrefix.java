6bbd15c0068d5700e7b94b1bc0a505380912ccc2
amareshhm <amareshhm92@gmail.com>
2022-04-23 12:38:20 +0530

    Update LongestCommonPrefix.java

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -22,3 +22,32 @@ public class LongestCommonPrefix {
 	}
 
 }
+
+//2nd Way:- Using sort
+
+
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return "";
+
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        int c = 0;
+        while (c < first.length()) {
+            if (first.charAt(c) == last.charAt(c))
+                c++;
+            else
+                break;
+        }
+        return c == 0 ? "" : first.substring(0, c);
+    }
+
+    public static void main(String[] args) {
+        String[] result = {"flower", "flow", "flight"};
+        System.out.println(longestCommonPrefix(result));
+    }
+}
